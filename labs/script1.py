7da45a74e5ecfb18e83dafd54ac5468872b54244
Alessia <thj2zk@virginia.edu>
2025-02-28 18:02:50 -0500

    add files

---
+++ labs/script1.py
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+LOCAL_FILE=$1
+BUCKET_NAME=$2
+EXPIRATION_TIME=$3
+
+
+#upload file
+aws s3 cp "$LOCAL_FILE" s3://"$BUCKET_NAME"/
+
+
+#presigned url
+aws s3 presign --expires-in "$EXPIRATION_TIME" s3://"$BUCKET_NAME"/"$LOCAL_FILE"
